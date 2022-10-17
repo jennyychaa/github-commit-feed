ae99c20a5c65400522bad2613e5c397b44532f93
Jenny Cha <jenny.cha@heyfavor.com>
2022-10-17 00:58:43 -0400

    Adding placeholder tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// import { Search } from './pages';
+
+// @TODO Write test cases for the following
+// test('renders the search page upon app the loads', () => {
+//   render(<Search />);
+//   const title = screen.getByText(/Search Github Repository Logs.../i);
+//   expect(title).toBeInTheDocument();
+// });
+
+// test('shows a list of Git commits upon search', () => {
+// });
+
+// test('load more Git commits upon clicking Load More button', () => {
+// });
+
+// test('shows an error page if Github repositiory does not exist', () => {
+// });
+
+// test('shows an error page if navigated to non-existent page', () => {
+// });
